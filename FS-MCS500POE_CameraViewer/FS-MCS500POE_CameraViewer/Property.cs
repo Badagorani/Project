87b90d8b66a7a767e4fa151cc7965ce62ddfd9ef
Badagorani <dgchoi@seinens.com>
2023-01-10 18:04:56 +0900

    현재 작업 중인 프로젝트

--- FS-MCS500POE_CameraViewer/FS-MCS500POE_CameraViewer/Property.cs
+++ FS-MCS500POE_CameraViewer/FS-MCS500POE_CameraViewer/Property.cs
@@ -38,13 +38,13 @@ namespace FS_MCS500POE_CameraViewer
 
 			XmlNode userinfo = xmlFile.CreateElement("사용자_정보");
 			XmlNode username = xmlFile.CreateElement("사용자_이름");
-			username.InnerText = ps._sUser;
+			username.InnerText = ps.m_sUser;
 			userinfo.AppendChild(username);
 			XmlNode userpermission = xmlFile.CreateElement("사용자_권한");
-			userpermission.InnerText = ps._sPermission;
+			userpermission.InnerText = ps.m_sPermission;
 			userinfo.AppendChild(userpermission);
 			XmlNode UseStartTime = xmlFile.CreateElement("사용시작시간");
-			UseStartTime.InnerText = ps._sStartTime;
+			UseStartTime.InnerText = ps.m_sStartTime;
 			userinfo.AppendChild(UseStartTime);
 			XmlNode UseEndTime = xmlFile.CreateElement("사용종료시간");
 			UseEndTime.InnerText = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
@@ -73,9 +73,6 @@ namespace FS_MCS500POE_CameraViewer
 	public class Property_Setting
 	{
 		Form1 form;
-		public string _sPermission = "";
-		public string _sStartTime = "";
-		public string _sEndTime = "";
 		public Property_Setting(Form1 form)
 		{
 			this.form = form;
@@ -90,49 +87,74 @@ namespace FS_MCS500POE_CameraViewer
 			XmlNodeList nodes = xmlFile.SelectNodes("/전체/사용자_정보");
 			foreach(XmlNode node in nodes)
 			{
-				_sUser = node.SelectSingleNode("사용자_이름").InnerText;
-				if (_sUser.Equals("홍길동")) _sPermission = "관리자";
-				else _sPermission = "일반 유저";
-				_sStartTime = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+				m_sUser = node.SelectSingleNode("사용자_이름").InnerText;
+				if (m_sUser.Equals("홍길동")) m_sPermission = "관리자";
+				else m_sPermission = "일반 유저";
+				m_sStartTime = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
 			}
 			nodes = xmlFile.SelectNodes("/전체/카메라_정보");
 			foreach (XmlNode node in nodes)
 			{
-				//_sCamera1_DeviceIP = node.SelectSingleNode("카메라1_IP주소").InnerText;
-				//_sCamera2_DeviceIP = node.SelectSingleNode("카메라2_IP주소").InnerText;
-				//_sCamera3_DeviceIP = node.SelectSingleNode("카메라3_IP주소").InnerText;
-				_sCamera1_DeviceIP = form.Camera1IP;
-				_sCamera2_DeviceIP = form.Camera2IP;
-				_sCamera3_DeviceIP = form.Camera3IP;
+				m_sCamera1_DeviceIP = form.Camera1IP;
+				m_sCamera2_DeviceIP = form.Camera2IP;
+				m_sCamera3_DeviceIP = form.Camera3IP;
 			}
 		}
 		//[Category("카테고리 없으면 기타로 들어감"), DisplayName("왼쪽의 이름"), Description("맨밑에 설명")]
+		public string m_sUser;
 		[Category("\t사용자 정보"), DisplayName("이름"), Description("사용자의 성함")]
-		public string _sUser { get; set; }
+		public string User
+		{
+			get { return m_sUser; }
+			//set { m_sUser = value; }
+		}
+
+		public string m_sPermission;
 		[Category("\t사용자 정보"), DisplayName("권한"), Description("사용자에 따라 달라지는 권한")]
 		public string Permission
 		{
-			get { return _sPermission; }
-			//set { if (IsEdit) _sPermission = value; }
+			get { return m_sPermission; }
+			//set { m_sPermission = value; }
 		}
+
+		public string m_sStartTime;
 		[Category("\t사용자 정보"), DisplayName("사용시작시간"), Description("사용을 시작한 시간")]
 		public string StartTime
 		{
-			get { return _sStartTime; }
-			//set { if (IsEdit) StartTime = value; }
+			get { return m_sStartTime; }
+			//set { m_sStartTime = value; }
 		}
+
+		public string m_sEndTime;
 		[Category("\t사용자 정보"), DisplayName("사용종료시간"), Description("사용을 종료한 시간")]
 		public string EndTime
 		{
-			get { return _sEndTime; }
-			//set { if (IsEdit) EndTime = value; }
+			get { return m_sEndTime; }
+			//set { m_sEndTime = value; }
 		}
+		public string m_sCamera1_DeviceIP;
 		[Category("카메라 정보"), DisplayName("카메라1 IP 주소"), Description("카메라에 설정된 IP")]
-		public string _sCamera1_DeviceIP { get; set; }
+		public string Camera1_DeviceIP
+		{
+			get { return m_sCamera1_DeviceIP; }
+			set { m_sCamera1_DeviceIP = value; }
+		}
+
+		public string m_sCamera2_DeviceIP;
 		[Category("카메라 정보"), DisplayName("카메라2 IP 주소"), Description("카메라에 설정된 IP")]
-		public string _sCamera2_DeviceIP { get; set; }
+		public string Camera2_DeviceIP
+		{
+			get { return m_sCamera2_DeviceIP; }
+			set { m_sCamera2_DeviceIP = value; }
+		}
+
+		public string m_sCamera3_DeviceIP;
 		[Category("카메라 정보"), DisplayName("카메라3 IP 주소"), Description("카메라에 설정된 IP")]
-		public string _sCamera3_DeviceIP { get; set; }
+		public string Camera3_DeviceIP
+		{
+			get { return m_sCamera3_DeviceIP; }
+			set { m_sCamera3_DeviceIP = value; }
+		}
 		//[Category("레이아웃"), DisplayName("가로 크기"), Description("센서로 인식하는 가로 크기")]
 		//public int _iSensorWidth { get; }
 		//[Category("레이아웃"), DisplayName("세로 크기"), Description("센서로 인식하는 세로 크기")]
